e6cad568d2cb5ea2606897eae62e87434a282450
wangqing <350051556@qq.com>
2018-06-11 21:51:51 +0800

    add BannerView

--- app/src/main/java/com/qgg/practice/MainActivity.java
+++ app/src/main/java/com/qgg/practice/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.qgg.practice.retrofitdemo.RetrofitTestActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,5 +41,6 @@ public class MainActivity extends AppCompatActivity {
     public void practiceDraw7(View view) {
         //startActivity(new Intent(this, PracticeDraw7Activity.class));
         startActivity(new Intent(this, ParallaxActivity.class));
+        Glide.with(this).load("").into(new ImageView(this));
     }
 }
